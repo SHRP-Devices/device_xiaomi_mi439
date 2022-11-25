b843d2c3273c1be33e9abaeec56800b9ad592350
rxuglr <rxuglr@mail.ru>
2022-11-25 18:44:34 +0300

    SHRP bringup

--- twrp_mi439.mk
+++ twrp_mi439.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/xiaomi/mi439/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
